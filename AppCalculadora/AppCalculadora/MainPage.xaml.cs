7465db6cce8f923238cafea51bac3a9f4adcdb92
TIM-JUS <1440406@senati.pe>
2023-09-21 15:30:22 -0500

    Botones de numeros

---
+++ AppCalculadora/AppCalculadora/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        private string currentInput = "";
+        private string currentOperator = "";
+        private double result = 0.0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnNumberButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            resultLabel.Text = currentInput;
+        }
+        private void OnOperatorButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentOperator = button.Text;
+            result = double.Parse(currentInput);
+            currentInput = "";
+        }
+        private void OnEqualButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentOperator) && !string.IsNullOrEmpty(currentInput))
+            {
+                double secondOperand = double.Parse(currentInput);
+                switch (currentOperator)
+                {
+                    case "+":
+                        result += secondOperand;
+                        break;
+                    case "-":
+                        result -= secondOperand;
+                        break;
+                    case "*":
+                        result *= secondOperand;
+                        break;
+                    case "/":
+                        if (secondOperand != 0)
+                            result /= secondOperand;
+                        else
+                            resultLabel.Text = "Error";
+                        break;
+                }
+                resultLabel.Text = result.ToString();
+                currentInput = "";
+                currentOperator = "";
+            }
+        }
+        private void OnClearButtonClicked(object sender, EventArgs e)
+        {
+            currentInput = "";
+            currentOperator = "";
+            result = 0.0;
+            resultLabel.Text = "0";
+        }
+
+    }
+}
+
+
+
+
